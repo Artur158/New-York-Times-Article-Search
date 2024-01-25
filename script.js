146d3559f8392dace1451f5cc4706678fbdd6e3e
Artur <artur.piasecki158@gmail.comgit config --global user.email artur.piasecki158@gmail.comgit config --list$ git config --global user.email artur.piasecki158@gmail.com>
2024-01-25 21:26:21 +0000

    newCodeNewHeadline

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
 
- * @returns {string} URL for NYT API based on form inputs
- */
+
+@returns {string} URL for NYT API based on form inputs
+
 function buildQueryURL() {
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-    var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
-      .val()
-      .trim();
+
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+
+
+  var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
+
+
+  queryParams.q = $("#search-term")
+    .val()
+    .trim();
+
+
+  var startYear = $("#start-year")
+    .val()
+    .trim();
+
+  if (parseInt(startYear)) {
+    queryParams.begin_date = startYear + "0101";
+  }
+
+
+  var endYear = $("#end-year")
+    .val()
+    .trim();
+
+  if (parseInt(endYear)) {
+    queryParams.end_date = endYear + "0101";
+  }
+
+
+  console.log("---------------\nURL: " + queryURL + "\n---------------");
+  console.log(queryURL + $.param(queryParams));
+  return queryURL + $.param(queryParams);
+}
+
+
+
+@param {object} NYTData - object containing NYT API data
+
+function updatePage(NYTData) {
+
+  var numArticles = $("#article-count").val();
+
+
+  console.log(NYTData);
+  console.log("------------------------------------");
 }
