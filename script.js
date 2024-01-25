4c86567638b650f9fcd86c6f643e288fa176bc87
RusLena <RusLena@users.noreply.github.com>
2024-01-25 22:52:28 +0000

    Register the Submit button

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-/**
- * pulls information from the form and build the query URL
- * @returns {string} URL for NYT API based on form inputs
- */
 function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-  
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
-      .val()
-      .trim();
+  // queryURL is the url we'll use to query the API
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+  // Begin building an object to contain our API call's query parameters
+  // Set the API key
+  var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
+
+
 }
+
+//-----------------------------------------------------------------//
+
+
+
+
+// CLICK HANDLERS
+// ==========================================================
+
+$("#run-search").on("click", function (event) {
+  event.preventDefault();
+  clear();
+  var queryURL = buildQueryURL();
+
+  
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(updatePage);
+});
+
+//  .on("click") function associated with the clear button
+$("#clear-all").on("click", clear);
